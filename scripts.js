fb41e3156feb0b82944ffa9563b47121ecff4c31
Bss <mx.bss@proton.me>
2025-06-03 00:27:17 +0200

    add reset function

--- scripts.js
+++ scripts.js
@@ -1,17 +1,29 @@
 const selButtons = document.querySelector(".selection");
 
-const roundInfosDiv = document.querySelector(".round-info")
+const roundInfosDiv = document.querySelector(".round-info");
 const currentRoundDiv = document.querySelector(".current");
 
 const userChoiceDiv = document.querySelector(".user-choice");
 const computerChoiceDiv = document.querySelector(".computer-choice");
 
 const resultDiv = document.querySelector(".result");
+const userScoreSpan = document.querySelector("#player-score");
+const computerScoreSpan = document.querySelector("#computer-score");
 
 let computerScore = 0;
 let userScore = 0;
 let currentRound = 1;
 let gameWinner = null;
+
+reset();
+
+function reset() {
+  computerScore = 0;
+  userScore = 0;
+  currentRound = 1;
+  gameWinner = null;
+  updateScore();
+}
 
 function getComputerChoice() {
   let choice = Math.round(Math.random() * 2);
@@ -27,9 +39,8 @@ function getComputerChoice() {
 }
 
 function getRoundWinner(computerChoice, userChoice) {
-
   if (computerChoice === userChoice) {
-    return "tie"
+    return "none";
   }
 
   if (computerChoice === "rock") {
@@ -68,12 +79,17 @@ function updateRoundInfos(computerChoice, userChoice, result) {
   resultDiv.textContent = `${result}`;
 }
 
+function updateScore() {
+  userScoreSpan.textContent = `${userScore}`;
+  computerScoreSpan.textContent = `${computerScore}`;
+}
+
 function playRound(userChoice) {
   let computerChoice = getComputerChoice();
   let roundWinner = getRoundWinner(computerChoice, userChoice);
   let result;
 
-  if (roundWinner === "tie") {
+  if (roundWinner === "none") {
     result = "It's a tie !";
   }
   if (roundWinner === "user") {
@@ -85,35 +101,27 @@ function playRound(userChoice) {
     computerScore++;
   }
 
-  console.log(`${roundWinner} wins this round !`);
-  console.log(
-    `Your score     : ${userScore}\n` +
-    `Computer score : ${computerScore}`);
-
   updateRoundInfos(computerChoice, userChoice, result);
-  currentRound++;
+  updateScore();
 
+  currentRound++;
   return roundWinner;
 }
 
 function play(userChoice) {
-
   console.log("Match is over");
 
   if (computerScore > userScore) {
     gameWinner = "computer";
     console.log("You lost..");
-  }
-  else if (computerScore < userScore) {
+  } else if (computerScore < userScore) {
     gameWinner = "user";
     console.log("You win :)");
-  }
-  else {
+  } else {
     console.log("It's a tie !");
   }
 }
 
-selButtons.addEventListener('click', (event) => {
+selButtons.addEventListener("click", (event) => {
   playRound(event.target.id);
-})
-
+});
